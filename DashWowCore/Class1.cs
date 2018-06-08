0f19f0587f7edd5699dba3526938e93635404cd8
Yerson <Yerson.contacto@gmail.com>
2018-06-08 16:15:32 -0400

    Setting inicial, no estable

---
+++ DashWowCore/Class1.cs
@@ -0,0 +1,20 @@
+﻿using WowDotNetAPI;
+using WowDotNetAPI.Models;
+
+namespace DashWowCore
+{
+    public class Class1
+    {
+
+        private static WowExplorer explorer;
+        private static Guild guild;
+        private static string APIKey = "p4bnuju7grf3qfdcgbpwec8ebf5hq29g";
+
+
+        public Guild GetMyGuild()
+        {
+            var myGuild = explorer.GetGuild(Region.US, "Stormrage", "Serenity Later");
+            return myGuild;
+        }
+    }
+}
---
+++ DashWowCoreTests/Class1Tests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WowDotNetAPI;
+
+namespace DashWowCore.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        private static WowExplorer explorer;
+        private static string APIKey = "p4bnuju7grf3qfdcgbpwec8ebf5hq29g";
+
+        [TestMethod()]
+        public void GetMyGuildTest()
+        {
+            var myGuild = explorer.GetGuild(Region.US, "Stormrage", "Serenity Later");
+            Assert.IsNotNull(myGuild);
+            Assert.Fail();
+        }
+    }
+}
